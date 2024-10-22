37acae91616b95729ff6ca8f3150ab3524d94ee1
Erick Araya <95132759+Francolin09@users.noreply.github.com>
2024-10-22 11:38:43 -0300

    company actualizada
    company actualizada

--- gipit-back/app/api/company/route.ts
+++ gipit-back/app/api/company/route.ts
@@ -1,1 +1,28 @@
 
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+// Obtener todas las jefaturas (GET)
+export async function GET() {
+  try {
+    const managements = await prisma.company.findMany();
+    return NextResponse.json(managements);
+  } catch (error) {
+    return NextResponse.json({ error: 'Error fetching managements' }, { status: 500 });
+  }
+}
+
+// Crear una nueva jefatura (POST)
+export async function POST(req: NextRequest) {
+  const { company_id, name, description } = await req.json();
+  try {
+    const company = await prisma.company.create({
+      data: { name, description },
+    });
+    return NextResponse.json(company, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Error creating company' }, { status: 500 });
+  }
+}
